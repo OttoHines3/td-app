4fd851e4341214bf02c1172cfb1c37581e0cee6e
Otto Hines <ottohines@Ottos-MacBook-Pro-2.local>
2022-10-31 02:30:51 -0400

    I

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,6 @@ function App(){
         containerClassName={'pagination'}
         activeClassName={'active'}
       />
-     {/* <CommandPallete data={flattenedData}/> */}
     </div>
      
      );
